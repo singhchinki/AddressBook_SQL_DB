502cf6f30cf0c1fc295ceb3823fd01cdc55f9924
chinki kumari singh <singhchinkikumari.com>
2022-06-19 09:30:20 +0530

    [Chinki] Create Table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE Address_Book
+use Address_Book
+CREATE TABLE AddressBook
+(
+FirstName varchar(20),
+FastName varChar(20),
+Address varchar(100),
+City varchar(20),
+State varchar(20),
+Zip int,
+PhoneNumber varchar(12),
+Email varchar(20)
+);
+select * from AddressBook
